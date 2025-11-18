2f9aff6edb9e1bcbbf2b354eef1b1b7f7c94752f
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2025-11-18 21:05:46 +0200

    Start js

---
+++ js/main.js
@@ -0,0 +1,39 @@
+console.log("This is external file Hello JS")
+
+// One line comment
+
+/*
+Multi lines comment
+
+var x;
+
+x = 15;
+*/
+
+// Number
+var x = 26,
+    y = 44,
+    z = x + y;
+
+console.log(z)
+console.log(typeof z)
+
+
+// String
+var username = "Mohamed";
+
+console.log(username);
+console.log(typeof username);
+
+// Boolean
+var isVisited = false;
+console.log(isVisited)
+console.log(typeof isVisited)
+
+// Undefined
+var first;
+console.log(first)
+console.log(typeof first)
+
+// Null
+console.log(document.getElementById('test'))
