c600fb61d98ccb881257636f83fbb9f9116e38bd
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2026-01-03 21:52:59 +0200

    Tailwind and Ajax

--- js/session.js
+++ js/session.js
@@ -16,4 +16,38 @@ console.log(JSON.parse(jsonPerson))
 // XHR  : XML HTTP Request
 // XML  : extensible  markup language
 // HTTP : Hyper text transfer protocol
-// AJAX : asynchronous javascript and XML
+// AJAX : Asynchronous javascript and XML
+
+/*
+0- Request not initialized
+1- Server connection created
+2- Request received
+3- Request processing
+4- Request ready
+*/
+
+let usersRequest = new XMLHttpRequest()
+
+// console.log(usersRequest)
+let output = document.querySelector('#output')
+
+usersRequest.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+        let res = JSON.parse(this.response)
+        console.log(res)
+        output.nextElementSibling.classList.add('d-none')
+        for (let index = 0; index < res.length; index++) {
+            const user = res[index];
+            output.innerHTML += `<li class="list-group-item">${user.name}</li>`
+        }
+    }
+}
+
+usersRequest.open("GET", "https://jsonplaceholder.typicode.com/users")
+
+usersRequest.send()
+
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(res => console.log(res))
